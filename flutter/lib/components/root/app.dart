79619fdf5a96970b3ad9c5f22f5baeaee68f092e
bokotomo <bokotomo@me.com>
2023-02-22 23:34:55 +0900

    feat: hooks

--- flutter/lib/components/root/app.dart
+++ flutter/lib/components/root/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:flutter_hooks/flutter_hooks.dart' show HookWidget;
+import 'package:flutter_hooks/flutter_hooks.dart' show useState, useEffect;
 import 'package:gamer_reflection/modules/database/driver/sqlite.dart'
     show initDatabase, DBConnection;
 import 'package:gamer_reflection/components/layouts/tabbar/tabbar.dart'
@@ -9,39 +11,31 @@ import 'package:gamer_reflection/components/layouts/tabbar/tabbar.dart'
 const title = 'Gamer Reflection';
 
 /// ページ: タスク一覧
-class App extends StatefulWidget {
-  const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-/// _PageTaskState
-class _AppState extends State<App> {
-  final ValueNotifier<bool> canDC = ValueNotifier<bool>(false);
-
-  /// 初期
-  Future<void> setUp() async {
-    Database db = await initDatabase();
-    GetIt.I.registerSingleton<DBConnection>(DBConnection(db: db));
-  }
-
-  ///
-  Future<void> setData() async {
-    await setUp();
-    setState(() {
-      canDC.value = true;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    setData();
-  }
+class App extends HookWidget {
+  const App({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
+    ValueNotifier<bool> canDC = useState<bool>(false);
+
+    /// 初期
+    Future<void> setUp() async {
+      Database db = await initDatabase();
+      GetIt.I.registerSingleton<DBConnection>(DBConnection(db: db));
+    }
+
+    ///
+    Future<void> setData() async {
+      await setUp();
+      canDC.value = true;
+    }
+
+    useEffect(() {
+      setData();
+    }, []);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: title,
